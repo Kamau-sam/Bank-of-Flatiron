9cec94597f0b5ab844cacd1f1b579345a7b6d179
Kamau-sam <skamau.6411@gmail.com>
2024-04-29 09:01:20 +0300

    Added a default array of transactions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,47 @@ import "./Components/AppStyles.css"; // Importing CSS styles
 
 // Main App component
 function App() {
-  const [transactions, setTransactions] = useState([]); // State for transactions
+  // State that has an array of transactions
+  const [transactions, setTransactions] = useState([
+    {
+      date: "2024-04-23",
+      description: "Groceries",
+      category: "Food",
+      amount: "693",
+    },
+    {
+      date: "2024-04-25",
+      description: "Movies",
+      category: "Entertainment",
+      amount: "210",
+    },
+    {
+      date: "2024-04-26",
+      description: "Fashion",
+      category: "T-shirts,Jeans and bag",
+      amount: "2350",
+    },
+
+    {
+      date: "2024-04-26",
+      description: "Gaming",
+      category: "Entatainment",
+      amount: "693",
+    },
+    {
+      date: "2024-04-25",
+      description: "Ugali & Choma",
+      category: "Food",
+      amount: "440",
+    },
+    {
+      date: "2024-04-28",
+      description: "Fashion",
+      category: "Hair cut and beard trim",
+      amount: "500",
+    },
+  ]);
+
   const [searchTerm, setSearchTerm] = useState(""); // State for search term
 
   // Handler for adding a new transaction
